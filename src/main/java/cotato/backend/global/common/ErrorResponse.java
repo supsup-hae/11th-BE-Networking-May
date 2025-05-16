af1aae7f3bdbc32681a27cfe53a02b2798ed514e
supsup_hae <zksdbstjq@gmail.com>
2025-05-16 15:29:49 +0900

    :recycle: refactor: ErrorResponse 클래스 수정 (#3)
    - ErrorResponse 클래스의 구조를 개선하여 코드 가독성과 유지보수성을 높였습니다.
    - 중복되는 필드 및 메서드를 정리하고, 예외 처리 시 응답 데이터의 일관성을 확보했습니다.

--- src/main/java/cotato/backend/global/common/ErrorResponse.java
+++ src/main/java/cotato/backend/global/common/ErrorResponse.java
@@ -1,18 +1,35 @@
 package cotato.backend.global.common;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
+
+import cotato.backend.global.error.ErrorCode;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 
 @Getter
-@Builder
 public class ErrorResponse {
-    private final boolean success;
-    private final String message;
-    private final String path;
-    private final LocalDateTime timestamp;
+	private final int status;
+	private final String error;
+	private final String message;
+	private final String path;
+	private final LocalDateTime timestamp;
+
+	@Builder
+	protected ErrorResponse(final ErrorCode code, final String message, final String path) {
+		this.timestamp = LocalDateTime.now();
+		this.status = code.getStatus().value();
+		this.error = HttpStatus.valueOf(code.getStatus().value()).getReasonPhrase();
+		this.message = message;
+		this.path = path;
+	}
+
+	public static ErrorResponse of(final ErrorCode code, final String message, final String path) {
+		return new ErrorResponse(code, message, path);
+	}
 }
