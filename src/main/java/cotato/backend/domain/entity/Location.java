4b5ccdb1e1cde8de506407921af8ec60ceaf58cb
supsup_hae <zksdbstjq@gmail.com>
2025-05-17 13:34:09 +0900

    ✨ feat: Location 엔티티 추가 (#6)
    - 위치 기능 구현을 위한 데이터 정의

---
+++ src/main/java/cotato/backend/domain/entity/Location.java
@@ -0,0 +1,38 @@
+package cotato.backend.domain.entity;
+
+import java.math.BigDecimal;
+
+import cotato.backend.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "location")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class Location extends BaseEntity {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private BigDecimal latitude;
+
+	@Column(nullable = false)
+	private BigDecimal longitude;
+
+	@Column(nullable = false, name = "is_pinned")
+	private Boolean isPinned = false;
+}
